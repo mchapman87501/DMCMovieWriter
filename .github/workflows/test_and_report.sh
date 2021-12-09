da17df7343e5b50c1688ab3e36acb7ccb2afd26d
Mitch Chapman <mchapman87501@gmail.com>
2021-12-09 13:18:22 -0700

    GITHUB_ACTION_PATH is supported only in composite actions.

--- .github/workflows/test_and_report.sh
+++ .github/workflows/test_and_report.sh
@@ -3,4 +3,4 @@ set -e -u
 
 swift test --enable-code-coverage
 swift demangle --compact <$(swift test --show-codecov-path) >test_coverage.json
-python3 ${GITHUB_ACTION_PATH}/print_coverage_report.py
+python3 ${GITHUB_WORKSPACE}/.github/workflows/print_coverage_report.py
